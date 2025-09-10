1b861a7eeb322f227475070215ebbb8b33d45f17
ScienceByte <xxscienceboyxx@gmail.com>
2025-09-10 17:04:54 -0400

    serial terminal test

--- PersonalSmartLocker1/PersonalSmartLocker1.ino
+++ PersonalSmartLocker1/PersonalSmartLocker1.ino
@@ -1,9 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
-  //testing last time
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+Serial.println("Hello world!");
+delay(1000); 
 }
