e53c69f502b994c7ff1575e85a613f85beb6fab2
jtedesco0146 <jtedesco0146@vt.edu>
2025-09-24 16:38:16 -0400

    Update PersonalSmartLocker1_first_test.ino

--- PersonalSmartLocker1/PersonalSmartLocker1_first_test.ino
+++ PersonalSmartLocker1/PersonalSmartLocker1_first_test.ino
@@ -1,5 +1,6 @@
-void setup() {
-  //Serial.begin(9600);
+  //Set an array for each number pressed to be stored in
+  int passInput[4];
+  int input = 0;
   //Set each key as a character
   char KEYS[] 
     {
@@ -12,28 +13,30 @@ void setup() {
     {1.51, 1.61}, {0.06, 0.16}, {0.40, 0.50}, {0.88, 0.98}, {1.0, 1.1}, 
     {2.66, 2.86}, {3.56, 3.76}, {0.0, 0.22}
   };
+
+void setup() {
+  //Serial.begin(9600);
 }
 
 void loop() {
-//Use for loop to iterate until the correct number of buttons are pressed. 
-for (i > 0; i = 4, i++);
-  //Read the value of input A5 to determine which key was pressed
-  int keyPressed = analogRead(A5);
-
-  //Convert the signal from A5 into a voltage
-  //Use double because it yields more decimal points than float
-  double voltage = keyPressed * (5.0 / 1023.0);
-  
-  //Set an array for each number pressed to be stored in
-  int passInput[4];
-
-  for (int j = 0; j < 13; j++)
+  //Use for loop to iterate until the correct number of buttons are pressed. 
+  for (int i = 0; i < 4; i++)
   {
-    if (voltage >= voltages[j][0] && voltage <= voltages[j][1])
+    //Read the value of input A5 to determine which key was pressed
+    int keyPressed = analogRead(A5);
+
+    //Convert the signal from A5 into a voltage
+    //Use double because it yields more decimal points than float
+    double voltage = keyPressed * (5.0 / 1023.0);
+
+    for (int j = 0; j < 13; j++)
     {
-      passInput[input] = KEYS[j];
-      input++;
-      break;
+      if (voltage >= voltages[j][0] && voltage <= voltages[j][1])
+      {
+        passInput[input] = KEYS[j];
+        input++;
+        break;
+      }
     }
   }
 }
