5ee6cd871b40dff12eaacbda5aafa11ea7d2b9fa
jtedesco0146 <jtedesco0146@vt.edu>
2025-09-29 14:20:20 -0400

    Update PersonalSmartLocker1_first_test.ino

--- PersonalSmartLocker1/PersonalSmartLocker1_first_test.ino
+++ PersonalSmartLocker1/PersonalSmartLocker1_first_test.ino
@@ -1,42 +1,56 @@
-  //Set an array for each number pressed to be stored in
-  int passInput[4];
-  int input = 0;
-  //Set each key as a character
-  char KEYS[] 
-    {
-      '1','2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'
-    };
-  //Set matrix to identify each voltage range
-  const double voltages[][2] = 
-  {
-    {0.60, 0.70}, {2.0, 2.2}, {2.95, 3.05}, {0.17, 0.27}, {0.77, 0.87}, 
-    {1.51, 1.61}, {0.06, 0.16}, {0.40, 0.50}, {0.88, 0.98}, {1.0, 1.1}, 
-    {2.66, 2.86}, {3.56, 3.76}, {0.0, 0.22}
-  };
+// Store each number pressed
+char passInput[4];
+int input = 0;
+char approvedPass[5];
+bool passwordSet = false;
+
+// Key mapping for the 4x3 keypad
+char KEYS[] = { '1','2','3','4','5','6','7','8','9','*','0','#' };
+
+// Voltage ranges
+const double voltages[][2] = {
+  {0.63, 0.66},   // '1' Row1, Col150
+  {1.36, 1.38},   // '2' Row1, Col390
+  {1.93, 1.94},   // '3' Row1, Col680
+  {0.21, 0.22},   // '4' Row2, Col150
+  {0.51, 0.52},   // '5' Row2, Col390
+  {0.80, 0.81},   // '6' Row2, Col680
+  {0.10, 0.11},   // '7' Row3, Col150
+  {0.26, 0.27},   // '8' Row3, Col390
+  {0.42, 0.43},   // '9' Row3, Col680
+  {0.048, 0.05},  // '*' Row4, Col150
+  {0.12, 0.13},   // '0' Row4, Col390
+  {0.20, 0.21}    // '#' Row4, Col680
+};
+
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
+  Serial.println("Set passcode: "); //Prompts the user to input password
 }
 
 void loop() {
-  //Use for loop to iterate until the correct number of buttons are pressed. 
-  for (int i = 0; i < 4; i++)
-  {
-    //Read the value of input A5 to determine which key was pressed
-    int keyPressed = analogRead(A5);
+  int keyPressed = analogRead(A5);
+  double voltage = keyPressed * (5.0 / 1023.0);
 
-    //Convert the signal from A5 into a voltage
-    //Use double because it yields more decimal points than float
-    double voltage = keyPressed * (5.0 / 1023.0);
+  // Find which key matches the measured voltage
+  for (int j = 0; j < 12; j++) {
+    if (voltage >= voltages[j][0] && voltage <= voltages[j][1]) {
+      passInput[input] = KEYS[j];
+      input++;
+      Serial.print("Key pressed: ");
+      Serial.println(KEYS[j]);
+      delay(300); 
 
-    for (int j = 0; j < 13; j++)
-    {
-      if (voltage >= voltages[j][0] && voltage <= voltages[j][1])
-      {
-        passInput[input] = KEYS[j];
-        input++;
-        break;
+      if (input == 4) {
+        Serial.print("Passcode: ");
+        for (int i = 0; i < 4; i++) {
+          Serial.print(passInput[i]);
+        }
+        Serial.println();
+        input = 0; // reset for next code
       }
+      break;
     }
   }
 }
